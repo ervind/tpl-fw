861ecf980c40ab7fddf330c3aee18fbef21c31c0
ErvinDomonkos <ervin.domonkos@gmail.com>
2021-06-17 09:16:14 +0200

    Restricting demo options to be connected with "post" post_type only

--- framework/demo/metabox/metabox-sections.php
+++ framework/demo/metabox/metabox-sections.php
@@ -5,7 +5,7 @@ $tpl_section_args = [
 	"name"			=> 'simple_post_demos',
 	"title"			=> __( 'Metabox Simple Demo Fields', 'tpl' ),
 	"description"	=> __( 'Testing simple (non-repeater) fields', 'tpl' ),
-	"post_type"		=> [ 'post', 'chch_product' ],
+	"post_type"		=> [ 'post' ],
 ];
 TPL_FW()->register_section( $tpl_section_args );
 
@@ -14,7 +14,7 @@ $tpl_section_args = [
 	"name"			=> 'repeater_post_demos',
 	"title"			=> __( 'Metabox Repeater Demo Fields', 'tpl' ),
 	"description"	=> __( 'Testing repeater fields', 'tpl' ),
-	"post_type"		=> [ 'post', 'chch_product' ],
+	"post_type"		=> [ 'post' ],
 ];
 TPL_FW()->register_section( $tpl_section_args );
 
@@ -23,7 +23,7 @@ $tpl_section_args = [
 	"name"			=> 'combined_post_demos',
 	"title"			=> __( 'Metabox Combined Demo Fields', 'tpl' ),
 	"description"	=> __( 'Testing combined fields', 'tpl' ),
-	"post_type"		=> [ 'post', 'chch_product' ],
+	"post_type"		=> [ 'post' ],
 ];
 TPL_FW()->register_section( $tpl_section_args );
 
@@ -32,7 +32,7 @@ $tpl_section_args = [
 	"name"			=> 'combined_repeater_post_demos',
 	"title"			=> __( 'Metabox Combined Repeater Demo Fields', 'tpl' ),
 	"description"	=> __( 'Testing combined repeater fields', 'tpl' ),
-	"post_type"		=> [ 'post', 'chch_product' ],
+	"post_type"		=> [ 'post' ],
 ];
 TPL_FW()->register_section( $tpl_section_args );
 
@@ -41,6 +41,6 @@ $tpl_section_args = [
 	"name"			=> 'conditional_post_demos',
 	"title"			=> __( 'Conditional Demo Fields', 'tpl' ),
 	"description"	=> __( 'Conditional fields\' visibilities are dependent on other fields.', 'tpl' ),
-	"post_type"		=> [ 'post', 'chch_product' ],
+	"post_type"		=> [ 'post' ],
 ];
 TPL_FW()->register_section( $tpl_section_args );
